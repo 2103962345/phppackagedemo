752438a6cdff8027b8ada3cf92003f4219e3b340
李少江 <980916400@qq.com>
2017-09-19 21:30:51 +0800

    refine

---
+++ app/Demo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lishaojiang
+ * Date: 2017/9/19
+ * Time: 下午9:22
+ */
+class Demo
+{
+    public function run (){
+        echo "Demo run";
+    }
+}
